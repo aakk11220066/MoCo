e10e0a3f2e465f33f6f8d30e95f3310bfe16c48b
uriyallow14 <uriyallow14@gmail.com>
2022-01-05 18:50:44 +0200

    Update MoCoTrainer

--- MoCoTrainer.py
+++ MoCoTrainer.py
@@ -13,11 +13,14 @@ from DataHandling import DataLoaderCyclicIterator
 def program_log(msg):
     print(colored(msg, 'blue'))
 
+
 def minibatch_log(msg):
     print(colored(msg, 'green'))
 
+
 def matching_accuracy(probabilities: torch.Tensor, real_match_idx=0):
     return (torch.multinomial(input=probabilities, num_samples=1) == real_match_idx).sum() / probabilities.shape[0]
+
 
 def update_learning_rate(optimizer: torch.optim, epoch: int, total_epochs: int):
     if epoch in [total_epochs * 0.6, total_epochs * 0.8]:
@@ -34,7 +37,7 @@ def get_MoCo_feature_extractor(
     """
     Generates a feature extraction network as described by MoCo v2 paper based on the ResNet50 feature extractor backbone
     :param temperature: hyperparameter defining the density of the contrastive loss function
-    :param loader: unlabeled training data loader
+    :param loader: unlabeled training data loader ################################################################################################# need to UPDATE
     :param augment: augmentation function (random augmentation)
     :param momentum: hyperparameter defining the speed at which the key dictionary is updated
     :param key_dictionary_size: hyperparameter defining the number of keys to maintain.  Should be a   product of the loader batch_size
@@ -49,62 +52,86 @@ def get_MoCo_feature_extractor(
     program_log("Initializing feature extractor training")
     device = "cuda" if torch.cuda.is_available() else "cpu"
     f_q = get_encoder().to(device)
-    optimizer = torch.optim.SGD(f_q.parameters(), lr=0.03, weight_decay=1e-4, momentum=0.9)
-    f_k = copy.deepcopy(f_q)  # create independent copy of f_q that begins with the same parameters but updates more slowly
+    f_k = copy.deepcopy(f_q).to(
+        device)  # create independent copy of f_q that begins with the same parameters but updates more slowly
+    optimizer = torch.optim.SGD(f_q.parameters(), lr=0.01, weight_decay=1e-4, momentum=0.9)
     loss_fn = torch.nn.CrossEntropyLoss()
 
     # Generate keys_queue
     program_log("Generating initial keys queue")
     num_initial_key_batches = key_dictionary_size // loader.batch_size
     loader_iterator = DataLoaderCyclicIterator(loader, load_labels=False)
+
     with torch.no_grad():
-        keys_queue = torch.cat([
-            f_k(
-                augment(next(loader_iterator))
-            )
-            for _ in tqdm(range(num_initial_key_batches))
-        ], dim=0)
+        for _ in tqdm(range(num_initial_key_batches)):
+            init_k = f_k(augment(next(loader_iterator).to(device))).detach()
+            init_k = torch.div(init_k, torch.norm(init_k, dim=1).reshape(-1, 1))
+            keys_queue = torch.cat([init_k], dim=0)
 
     program_log("Beginning training loop")
+    f_q.train()
     for epoch in tqdm(range(num_epochs)):
         update_learning_rate(optimizer, epoch, num_epochs)
-        for x in loader_iterator:  # load a minibatch x with N samples
+        total_loss = 0.0
+        print(f'epoch = {epoch + 1}')
+        for batch, x in enumerate(loader_iterator):  # load a minibatch x with N samples
+
             optimizer.zero_grad()
 
-            x_q = augment(x)  # a randomly augmented version
-            x_k = augment(x)  # another randomly augmented version
-            q = f_q(x_q)  # queries: NxC
+            x_q = augment(x).to(device)  # a randomly augmented version
+            x_k = augment(x).to(device)  # another randomly augmented version
+            q = f_q(x_q).to(device)  # queries: NxC
+
             with torch.no_grad():  # no gradient to keys
-                k = f_k(x_k)  # keys: NxC
+                shuffled_idx = torch.randperm(x_k.size(dim=0))  # permutation of indexes
+                shuffled_x_k = x_k[shuffled_idx]  # shuffle keys
+                shuffled_k = f_k(shuffled_x_k)  # shuffled keys encoder
+                k = torch.zeros_like(shuffled_k)  # restore original keys order
+                k[shuffled_idx] = shuffled_k  # keys: NxC
+
+            # normalize query and keys
+            q = torch.div(q, torch.norm(q, dim=1).reshape(-1, 1))
+            k = torch.div(k, torch.norm(k, dim=1).reshape(-1, 1))
+
             minibatch_size, sample_size = k.shape
-            N, C, K = minibatch_size, sample_size, key_dictionary_size
+            N, C = minibatch_size, sample_size
 
             # positive logits: Nx1
             l_pos = torch.bmm(q.view(N, 1, C), k.view(N, C, 1)).squeeze(dim=2)
-            # negative logits: NxK
-            l_neg = torch.mm(q.view(N, C), keys_queue.view(C, K))
-            # logits: Nx(1+K)
-            logits = torch.cat([l_pos, l_neg], dim=1)
+            positive_key = torch.exp(torch.div(l_pos, temperature))
 
-            # contrastive loss, Eqn.(1)
-            labels = torch.zeros((N,), dtype=torch.long)  # positives are the 0-th
-            loss = loss_fn(logits/temperature, labels)
+            # negative logits: NxK
+            l_neg = torch.mm(q.view(N, C), torch.t(keys_queue))
+            negative_keys = torch.sum(torch.exp(torch.div(l_neg, temperature)), dim=1)
+
+            denominator = negative_keys + positive_key
+
+            # logits: Nx(1+K)
+            logits = torch.cat([positive_key, negative_keys.reshape(-1, 1)], dim=1).to(device)
+
+            loss = torch.mean(-torch.log(torch.div(positive_key, denominator)))
+            total_loss += loss.item()
 
             # SGD update: query network
             loss.backward()
             optimizer.step()
 
-            # momentum update: key network
             with torch.no_grad():
                 for target_param, old_param, new_param in zip(f_k.parameters(), f_k.parameters(), f_q.parameters()):
                     # Edit params in-place
-                    target_param.data = momentum*old_param + (1-momentum)*new_param
+                    target_param.data = momentum * old_param + (1 - momentum) * new_param
 
             # update dictionary
             keys_queue = torch.cat((keys_queue, k))  # enqueue the current minibatch
             keys_queue = keys_queue[k.shape[0]:]  # dequeue the earliest minibatch
 
-            minibatch_log(f"Completed minibatch, loss={loss.item()}, with accuracy={matching_accuracy(torch.softmax(logits, dim=1))}")
+            # print current batch and accuracy results
+            if batch % 100 == 0:
+                print(f'batch = {batch}')
+                minibatch_log(
+                    f"Completed minibatch, loss={loss.item()}, with accuracy={matching_accuracy(torch.softmax(logits, dim=1))}")
+
+        print(f'Epoch #{epoch + 1}: Avg. loss={total_loss / len(loader)}')
 
     program_log("Completed training MoCo feature extractor!")
     return f_q
