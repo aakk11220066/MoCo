f86e14dce6761b086d29d20f9e85da126389932d
uriyallow14 <uriyallow14@gmail.com>
2022-01-14 00:11:39 +0200

    Update Classifier

--- Linear_classifier.py
+++ Linear_classifier.py
@@ -2,20 +2,19 @@ import torch
 
 
 class Classifier(torch.nn.Module):
-    def __init__(self, feature_extractor: torch.nn.Module, loss_func=None, optimizer_type=None):
+    def __init__(self, feature_extractor: torch.nn.Module, num_classes=10):
         super(Classifier, self).__init__()
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.num_classes = num_classes
         self.feature_extractor = feature_extractor.to(device=device)
         self.fc = torch.nn.Sequential(
-            torch.nn.Linear(in_features=1000, out_features=10),
+            torch.nn.Linear(in_features=2048, out_features=self.num_classes, device=device),
             torch.nn.Softmax(dim=1)
-        )
-        # ftr_exctr_final_layer = self.feature_extractor.fc[-1]
-        # self.fc = torch.nn.Linear(in_features=ftr_exctr_final_layer.out_features,
-        #                           out_features=ftr_exctr_final_layer.out_features, device=device)
+        ).to(device)
 
     def forward(self, x):
         with torch.no_grad():
-            x = self.feature_extractor(x)  # Only fine-tune classifier head
+            x = self.feature_extractor(x)
+        x = torch.flatten(x, start_dim=1)
         x = self.fc(x)
         return x
