716934ffb601b0ca2645d5213f8574cea8580e6d
uriyallow14 <uriyallow14@gmail.com>
2022-01-15 18:05:32 +0200

    Update Main

--- Main.py
+++ Main.py
@@ -10,45 +10,38 @@ from Linear_classifier import Classifier
 from Classifier_trainer import ClassifierTrainer
 from Plot_results import plot_fit
 from FeatureExtractor import get_encoder
+import config as conf
 
 
-# Hyperparams taken from paper
-TEMPERATURE = 0.07
-MOMENTUM = 0.999
-KEY_DICTIONARY_SIZE = 4096
-NUM_EPOCHS = 1000
+train_loader, test_loader = get_loaders(data_path='Imagenette2', batch_size=256)
+# train_loader, test_loader = get_loaders(data_path=conf.path, batch_size=conf.batch_size)
 
-path = untar_data(URLs.IMAGENETTE)
-CHECKPOINT = 'final_model.pth'
-EARLY_STOP = 20
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-# train_loader, test_loader = get_loaders(data_path='Imagenette2', batch_size=32)
-train_loader, test_loader = get_loaders(data_path=path, batch_size=256)
-#
-# f_q = get_MoCo_feature_extractor(temperature=TEMPERATURE, loader=train_loader,
-#                                   augment=augment, momentum=MOMENTUM,
-#                                   key_dictionary_size=KEY_DICTIONARY_SIZE, num_epochs=NUM_EPOCHS)
-
-f_q = get_encoder().to(device)
-f_q.load_state_dict(torch.load('f_q_weights.pth', map_location=device))
+if conf.TRAIN_FEATURES:
+    f_q = get_MoCo_feature_extractor(temperature=conf.TEMPERATURE, loader=train_loader,
+                                      augment=augment, momentum=conf.MOMENTUM,
+                                      key_dictionary_size=conf.KEY_DICTIONARY_SIZE, num_epochs=conf.NUM_EPOCHS)
+else:
+    f_q = get_encoder().to(conf.device)
+    f_q.load_state_dict(torch.load('f_q_weights.pth', map_location=conf.device))
 new_f_q = torch.nn.Sequential(*(list(f_q.children())[:-1]))
 
 num_classes_ = len(train_loader.dataset.class_to_idx)
 
-imagenette_classifier = Classifier(feature_extractor=new_f_q, num_classes=num_classes_).to(device)
+imagenette_classifier = Classifier(feature_extractor=new_f_q, num_classes=num_classes_).to(conf.device)
 
-optimizer = torch.optim.SGD(imagenette_classifier.parameters(), lr=30, weight_decay=0, momentum=0.9)
+optimizer = torch.optim.SGD(imagenette_classifier.parameters(), lr=conf.lr, weight_decay=conf.weight_decay,
+                            momentum=conf.momentum)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
 
-loss_fn = torch.nn.CrossEntropyLoss().to(device)
+loss_fn = torch.nn.CrossEntropyLoss().to(conf.device)
 
-trainer = ClassifierTrainer(imagenette_classifier, loss_fn, optimizer)
+trainer = ClassifierTrainer(imagenette_classifier, loss_fn, optimizer, scheduler)
 
-fit_result = trainer.fit(train_loader, test_loader, num_epochs=NUM_EPOCHS, checkpoints=CHECKPOINT,
-                         early_stopping=EARLY_STOP)
+fit_result = trainer.fit(train_loader, test_loader, num_epochs=conf.NUM_EPOCHS, checkpoints=conf.CHECKPOINT,
+                         early_stopping=conf.EARLY_STOP)
 
 fig, axes = plot_fit(fit_result)
 
-fig.savefig('Fit_results.png')
+fig.savefig(f'Fit_results_{fit_result.num_epochs}.png')
 
 print('finished')
