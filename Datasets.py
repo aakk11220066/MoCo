b393158a5c4896bf63623d412644afed05aaa9fc
uriyallow14 <uriyallow14@gmail.com>
2022-01-16 18:50:07 +0200

    Update Datasets

--- Datasets.py
+++ Datasets.py
@@ -3,7 +3,7 @@ from torchvision import datasets, transforms
 from os.path import join
 
 
-def get_loaders(data_path, batch_size):
+def get_loaders(data_path, batch_size, shuffle=True):
     """
 
     :param data_path: path to the directory in which we have the train and validation files
@@ -23,8 +23,24 @@ def get_loaders(data_path, batch_size):
     ds_train = datasets.ImageFolder(join(data_path, 'train'), transform=transform_["train"])
     ds_val = datasets.ImageFolder(join(data_path, 'val'), transform=transform_["val"])
 
-    # Set num_workers = 0 if this causes crashes on your machine (it's dependent on how many subcores you have available)
-    dl_train = torch.utils.data.DataLoader(ds_train, batch_size, shuffle=True, num_workers=0)
-    dl_val = torch.utils.data.DataLoader(ds_val, batch_size, shuffle=True, num_workers=0)
+    train_size = len(ds_train)
+    moco_train_size = int(0.8 * train_size)
+    ds_train_moco, ds_train_clf = torch.utils.data.random_split(ds_train, [moco_train_size, train_size-moco_train_size])
 
-    return dl_train, dl_val
+    val_size = len(ds_val)
+    moco_val_size = int(0.8 * val_size)
+    ds_val_moco, ds_val_clf = torch.utils.data.random_split(ds_val, [moco_val_size, val_size - moco_val_size])
+
+    print(f"Training moco dataset has {len(ds_train_moco)} images with a total of {len(ds_train_moco) // batch_size + 1} batches")
+    print(f"Testing moco dataset has {len(ds_val_moco)} images with a total of {len(ds_val_moco) // batch_size + 1} batches")
+    print(f"Training clf dataset has {len(ds_train_clf)} images with a total of {len(ds_train_clf) // batch_size + 1} batches")
+    print(f"Testing clf dataset has {len(ds_val_clf)} images with a total of {len(ds_val_clf) // batch_size + 1} batches")
+
+    # Set num_workers = 0 if this causes crashes on your machine (it's dependent on how many subcores you have available)
+    dl_train_moco = torch.utils.data.DataLoader(ds_train_moco, batch_size, shuffle=shuffle, num_workers=0)
+    dl_val_moco = torch.utils.data.DataLoader(ds_val_moco, batch_size, shuffle=shuffle, num_workers=0)
+
+    dl_train_clf = torch.utils.data.DataLoader(ds_train_clf, batch_size, shuffle=shuffle, num_workers=0)
+    dl_val_clf = torch.utils.data.DataLoader(ds_val_clf, batch_size, shuffle=shuffle, num_workers=0)
+
+    return dl_train_moco, dl_val_moco, dl_train_clf, dl_val_clf
